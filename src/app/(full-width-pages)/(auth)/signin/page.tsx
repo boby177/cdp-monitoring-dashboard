5827089b9c8ee328a61637546c7285ade1afe3d6
Boby <boby.ms378@gmail.com>
2026-07-20 09:54:38 +0700

    feat: redesign sign in layout UI

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -2,8 +2,8 @@ import SignInForm from "@/components/auth/SignInForm";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Next.js SignIn Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Signin Page TailAdmin Dashboard Template",
+  title: "CDP Dashboard Sign In",
+  description: "This is the signin page for CDP Dashboard",
 };
 
 export default function SignIn() {
